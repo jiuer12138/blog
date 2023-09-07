e845713d424a6b46247b43a36818bf60eb8dc367
刘剑飞 <18815283494@163.com>
2023-09-07 15:46:40 +0800

    init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,10 @@
+import {defineConfig} from "vitepress";
+
+export default defineConfig({
+    title: '一个大不liu',
+    description: '个人博客',
+    base: '/blog/',
+    themeConfig: {
+        logo: '/logo.png',
+    },
+})
